f869a0e1eefc7f5a293250715921c4e5b0a45ff2
Brian Glogower <brian@tagged.com>
2011-07-18 17:12:46 -0700

    Starting to build netscaler-tools starting with setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='netscaler-tools',
+    version='1.0',
+    py_modules=['netscaler-tools'],
+
+    author = "Brian Glogower",
+    author_email = "bglogower@tagged.com",
+    description = "Repository of python functions for retrieving and modifying NetScaler settings."     
+)
