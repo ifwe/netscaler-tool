f219ef65b7d0d7c511b24f6e8f0f93f66517da23
Brian Glogower <bglogower@tagged.com>
2011-12-23 23:28:40 -0800

    Moved all def to netscalerapi.py.

--- netscaler/netscalerapi.py
+++ netscaler/netscalerapi.py
@@ -9,6 +9,7 @@ logging.basicConfig(level=logging.ERROR)
 logging.getLogger('suds.client').setLevel(logging.ERROR)
 logging.disable(logging.ERROR)
 
+
 def fetchPasswd(passwdFile):
     try:
         f = open(passwdFile,'r')
@@ -26,7 +27,6 @@ def fetchPasswd(passwdFile):
 
 
 def connection(host,wsdl):
-
     wsdlUrl = "http://%s/api/%s" % (host,wsdl)
     soapUrl = "http://%s/soap" % (host)
 
@@ -45,6 +45,21 @@ def connection(host,wsdl):
     return client
     
 
+def getConnected(host,wsdl,user,passwd):
+    try:
+        client = connection(host,wsdl)
+    except RuntimeError, e:
+        raise RuntimeError(e)
+
+    # Logging into NetScaler.
+    try:
+        login(client,user,passwd)
+    except RuntimeError, e:
+        raise RuntimeError(e)
+
+    return client
+
+
 def login(client, user, passwd):
     output = client.service.login(username=user, password=passwd)
     if output.rc != 0:
@@ -54,7 +69,6 @@ def login(client, user, passwd):
 
 
 def logout(client):
-
     output = client.service.logout() 
     if output.rc != 0 and output.rc != 1041:
         raise RuntimeError(output.message)
@@ -70,5 +84,156 @@ def runCmd(client, command, **args):
         return output.List
 
 
-def autoSave(client):
+def getListServices(client):
+    command = "getservice"
+    list = []
+
+    try:
+        output = runCmd(client,command)
+    except RuntimeError, e:
+        raise RuntimeError(e)
+
+    for entry in output:
+        list.append(entry.name)
+
+    list.sort()
+    return list
+
+
+def getListVservers(client):
+    command = "getlbvserver"
+    list = []
+
+    try:
+        output = runCmd(client,command)
+    except RuntimeError, e:
+        raise RuntimeError(e)
+
+    for entry in output:
+        list.append(entry.name)
+
+    list.sort()
+    return list
+
+
+def getServices(client,vserver):
+    command = "getlbvserver"
+    arg = {'name':vserver}
+
+    try:
+        output = runCmd(client,command,**arg)
+    except RuntimeError, e:
+        raise RuntimeError(e)
+
+    try:
+        return output[0].servicename
+    except AttributeError:
+        e = "Vserver %s doesn't have any service bound to it. You can probably delete it." % (vserver)
+        raise RuntimeError(e)
+
+
+def getStatServices(client,service):
+    command = "statservice"
+    arg = {'name':service}
+
+    try:
+        output = runCmd(client,command,**arg)
+    except RuntimeError, e:
+        raise RuntimeError(e)
+
+    return output[0].surgecount
+
+
+def getSurgeQueueSize(client,vserver):
+    msg = ""
+    wsdl = "NSStat.wsdl"
+    wsdlURL = "http://%s/api/%s" % (host,wsdl)
+    surgeCountTotal = 0
+
+    try:
+        output = getServices(client,vserver)
+    except RuntimeError, e:
+        raise RuntimeError(e)
+
+    # Since we got the services bound to the vserver in question, we now
+    # need to get surge queue count for each service, but that requires we
+    # change wsdl files.
+    try:
+        client = getConnected(host,wsdl,user,passwd)
+    except RuntimeError, e:
+        raise RuntimeError(e)
+
+    # Going through the list of services to get surge count.
+    for service in output:
+        if debug:
+            print "Fetching surge queue count for %s" % (service)
+
+        try:
+            output = getStatServices(client,service)
+        except RuntimeError, e:
+            raise RuntimeError(e)
+
+        if debug:
+            print "Surge count for %s: %s\n" % (service,output)
+
+        surgeCountTotal =+ int(output)
+
+    return surgeCountTotal
+
+
+def vserverExists(client, vserver):
+    command = "getlbvserver"
+    arg = {'name':vserver}
+
+    try:
+        output = netscalerapi.runCmd(client,command,**arg)
+    except RuntimeError, e:
+        raise RuntimeError(e)
+
+
+def addServer(server):
+    # Check if server resolves.
+    try:
+        socket.gethostbyname_ex(server)
+    except socket.gaierror, e:
+        msg = "%s does not resolve. Create DNS first." % (server)
+        raise RuntimeError(msg)
+
+    # command to add server entry.
+
+    # Need to gracefully handle the case if server already
+    # exists on netscaler.
+    
+
+def addService(server,proto,port,monitor,weight):
+    # Need to gracefully handle the case if service 
+    # already exists.
+
+    # Command to add service.
+
+    # This will need to set the correct monitor and weight if the vserver 
+    # already exists to match all other services with servers
+    # of the same specid.
+
     pass
+    
+
+def addLbVserver(vserver,service)
+    # Check if vserver resolves.
+    try:
+        socket.gethostbyname_ex(server)
+    except socket.gaierror, e:
+        msg = "%s does not resolve. Create DNS first." % (vserver)
+        raise RuntimeError(msg)
+
+    # Check if vserver exists on netscaler. If so
+    # stop and notify.
+
+
+def saveConfig(client):
+    command = "savensconfig"
+
+    try:
+        output = runCmd(client,command)
+    except RuntimeError, e:
+        raise RuntimeError(e)
