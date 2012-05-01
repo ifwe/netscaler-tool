78ec5ae4130f6ed48a4d7359017f3dc4ea0fd6da
Brian Glogower <bglogower@tagged.com>
2012-05-01 15:42:11 -0700

    Trying to make a class.

--- netscaler/netscalerapi.py
+++ netscaler/netscalerapi.py
@@ -11,230 +11,192 @@ logging.basicConfig(level=logging.ERROR)
 logging.getLogger('suds.client').setLevel(logging.ERROR)
 logging.disable(logging.ERROR)
 
+class Client(object):
 
-def fetchPasswd(passwdFile):
-    try:
-        f = open(passwdFile,'r')
-    except IOError, e:
-        raise IOError(e)
+    def fetchPasswd(passwdFile):
+        try:
+            f = open(passwdFile,'r')
+        except IOError, e:
+            raise IOError(e)
 
-    # Reading contents of passwd file.
-    passwd = f.readline().strip('\n')
+        # Reading contents of passwd file.
+        passwd = f.readline().strip('\n')
 
-    # Closing file handle
-    f.close()
+        # Closing file handle
+        f.close()
 
-    # Returning passwd
-    return passwd
-
-
-def login(host,user,passwd,debug):
-    #set the headers and the base URL
-    headers = {'Content-type': 'application/x-www-form-urlencoded'}
-    url = "http://%s/nitro/v1/config/" % (host)
-
-    #contruct the payload with URL encoding
-    payload = {"object":{"login":{"username":user,"password":passwd}}}
-    payload_encoded = urllib.urlencode(payload)
-
-    #create a HTTP object, and use it to submit a POST request
-    http = httplib2.Http()
-    response, content = http.request(url, 'POST', body=payload_encoded, headers=headers)
-
-    if debug:
-        #for debug purposes, print out the headers and the content of the response
-        print json.dumps(response, sort_keys=False, indent=4)
-        print json.dumps(content, sort_keys=False, indent=4)
-
-    data = json.loads(content)
-    sessionID = data["sessionid"]
-
-    # Returning sessionID
-    return sessionID
-    
-
-def logout(client):
-    output = client.service.logout() 
-    if output.rc != 0 and output.rc != 1041:
-        raise RuntimeError(output.message)
-    else:
-        return output.message
+        # Returning passwd
+        return passwd
 
 
-def getObject(host, sessionID, object):
+    def login(host,user,passwd,debug):
+        #set the headers and the base URL
+        headers = {'Content-type': 'application/x-www-form-urlencoded'}
+        url = "http://%s/nitro/v1/config/" % (host)
 
-    listOfVservers = []
+        #contruct the payload with URL encoding
+        payload = {"object":{"login":{"username":user,"password":passwd}}}
+        payload_encoded = urllib.urlencode(payload)
 
-    headers = {'Content-type': 'application/x-www-form-urlencoded', 'Cookie': 'sessionid='+sessionID}
-    url = "http://%s/nitro/v1/config/%s" % (host, object)
+        #create a HTTP object, and use it to submit a POST request
+        http = httplib2.Http()
+        response, content = http.request(url, 'POST', body=payload_encoded, headers=headers)
 
-    #create a HTTP object, and use it to submit a GET request
-    http = httplib2.Http()
-    response, content = http.request(url, 'GET', headers=headers)
-
-    data = json.loads(content)
-    errorcode = data["errorcode"]
-
-    if response.status != 200 or errorcode != 0:
-        raise RuntimeError(content)
-
-    return data
-
-
-def setObject(host, sessionID, object, value):
-    headers = {'Content-type': 'application/x-www-form-urlencoded', 'Cookie.sessionid': sessionID}
-    pass
-
-
-def getAllServices(client):
-    command = "getservice"
-    list = []
-
-    try:
-        output = runCmd(client,command)
-    except RuntimeError, e:
-        raise RuntimeError(e)
-
-    for entry in output:
-        list.append(entry.name)
-
-    list.sort()
-    return list
-
-
-def getAllVservers(client):
-    command = "getlbvserver"
-    list = []
-
-    try:
-        output = runCmd(client,command)
-    except RuntimeError, e:
-        raise RuntimeError(e)
-
-    for entry in output:
-        list.append(entry.name)
-
-    list.sort()
-    return list
-
-
-def getServicesBound(client,vserver):
-    command = "getlbvserver"
-    arg = {'name':vserver}
-
-    try:
-        output = runCmd(client,command,**arg)
-    except RuntimeError, e:
-        raise RuntimeError(e)
-
-    try:
-        return output[0].servicename
-    except AttributeError:
-        e = "Vserver %s doesn't have any service bound to it. You can probably delete it." % (vserver)
-        raise RuntimeError(e)
-
-
-def getStatServices(client,service):
-    command = "statservice"
-    arg = {'name':service}
-
-    try:
-        output = runCmd(client,command,**arg)
-    except RuntimeError, e:
-        raise RuntimeError(e)
-
-    return output[0].surgecount
-
-
-def getSurgeQueueSize(client,vserver):
-    wsdl = "NSStat.wsdl"
-    wsdlURL = "http://%s/api/%s" % (host,wsdl)
-    surgeCountTotal = 0
-
-    try:
-        services = getServicesBound(client,vserver)
-    except RuntimeError, e:
-        raise RuntimeError(e)
-
-    # Since we got the services bound to the vserver in question, we now
-    # need to get surge queue count for each service, but that requires we
-    # change wsdl files.
-    try:
-        client = getConnected(host,user,passwd)
-    except RuntimeError, e:
-        raise RuntimeError(e)
-
-    # Going through the list of services to get surge count.
-    for service in services:
         if debug:
-            print "Fetching surge queue count for %s" % (service)
+            #for debug purposes, print out the headers and the content of the response
+            print json.dumps(response, sort_keys=False, indent=4)
+            print json.dumps(content, sort_keys=False, indent=4)
+
+        data = json.loads(content)
+        sessionID = data["sessionid"]
+
+        # Returning sessionID
+        return sessionID
+        
+
+    def logout(client):
+        output = client.service.logout() 
+        if output.rc != 0 and output.rc != 1041:
+            raise RuntimeError(output.message)
+        else:
+            return output.message
+
+
+    def getObject(host, sessionID, object):
+
+        listOfVservers = []
+
+        headers = {'Content-type': 'application/x-www-form-urlencoded', 'Cookie': 'sessionid='+sessionID}
+        url = "http://%s/nitro/v1/config/%s" % (host, object)
+
+        #create a HTTP object, and use it to submit a GET request
+        http = httplib2.Http()
+        response, content = http.request(url, 'GET', headers=headers)
+
+        data = json.loads(content)
+        errorcode = data["errorcode"]
+
+        if response.status != 200 or errorcode != 0:
+            raise RuntimeError(content)
+
+        return data
+
+
+    def setObject(host, sessionID, object, value):
+        headers = {'Content-type': 'application/x-www-form-urlencoded', 'Cookie.sessionid': sessionID}
+        pass
+
+
+    def getAllServices(client):
+        command = "getservice"
+        list = []
 
         try:
-            output = getStatServices(client,service)
+            output = runCmd(client,command)
         except RuntimeError, e:
             raise RuntimeError(e)
 
-        if debug:
-            print "Surge count for %s: %s\n" % (service,output)
+        for entry in output:
+            list.append(entry.name)
 
-        surgeCountTotal =+ int(output)
-
-    return surgeCountTotal
-
-
-def getVserver(client, vserver):
-    command = "getlbvserver"
-    arg = {'name':vserver}
-
-    try:
-        output = runCmd(client,command,**arg)
-    except RuntimeError, e:
-        raise RuntimeError(e)
+        list.sort()
+        return list
 
 
-def addServer(server):
-    # Check if server resolves.
-    try:
-        socket.gethostbyname_ex(server)
-    except socket.gaierror, e:
-        msg = "%s does not resolve. Create DNS first." % (server)
-        raise RuntimeError(msg)
+    def getAllVservers(client):
+        command = "getlbvserver"
+        list = []
 
-    # command to add server entry.
+        try:
+            output = runCmd(client,command)
+        except RuntimeError, e:
+            raise RuntimeError(e)
 
-    # Need to gracefully handle the case if server already
-    # exists on netscaler.
-    
+        for entry in output:
+            list.append(entry.name)
 
-def addService(server,proto,port,monitor,weight):
-    # Need to gracefully handle the case if service 
-    # already exists.
-
-    # Command to add service.
-
-    # This will need to set the correct monitor and weight if the vserver 
-    # already exists to match all other services with servers
-    # of the same specid.
-
-    pass
-    
-
-def addLbVserver(vserver,service):
-    # Check if vserver resolves.
-    try:
-        socket.gethostbyname_ex(server)
-    except socket.gaierror, e:
-        msg = "%s does not resolve. Create DNS first." % (vserver)
-        raise RuntimeError(msg)
-
-    # Check if vserver exists on netscaler. If so
-    # stop and notify.
+        list.sort()
+        return list
 
 
-def saveConfig(client):
-    command = "savensconfig"
+    def getServicesBound(client,vserver):
+        command = "getlbvserver"
+        arg = {'name':vserver}
 
-    try:
-        output = runCmd(client,command)
-    except RuntimeError, e:
-        raise RuntimeError(e)
+        try:
+            output = runCmd(client,command,**arg)
+        except RuntimeError, e:
+            raise RuntimeError(e)
+
+        try:
+            return output[0].servicename
+        except AttributeError:
+            e = "Vserver %s doesn't have any service bound to it. You can probably delete it." % (vserver)
+            raise RuntimeError(e)
+
+
+    def getStatServices(client,service):
+        command = "statservice"
+        arg = {'name':service}
+
+        try:
+            output = runCmd(client,command,**arg)
+        except RuntimeError, e:
+            raise RuntimeError(e)
+
+        return output[0].surgecount
+
+
+    def getSurgeQueueSize(client,vserver):
+        wsdl = "NSStat.wsdl"
+        wsdlURL = "http://%s/api/%s" % (host,wsdl)
+        surgeCountTotal = 0
+
+        try:
+            services = getServicesBound(client,vserver)
+        except RuntimeError, e:
+            raise RuntimeError(e)
+
+        # Since we got the services bound to the vserver in question, we now
+        # need to get surge queue count for each service, but that requires we
+        # change wsdl files.
+        try:
+            client = getConnected(host,user,passwd)
+        except RuntimeError, e:
+            raise RuntimeError(e)
+
+        # Going through the list of services to get surge count.
+        for service in services:
+            if debug:
+                print "Fetching surge queue count for %s" % (service)
+
+            try:
+                output = getStatServices(client,service)
+            except RuntimeError, e:
+                raise RuntimeError(e)
+
+            if debug:
+                print "Surge count for %s: %s\n" % (service,output)
+
+            surgeCountTotal =+ int(output)
+
+        return surgeCountTotal
+
+
+    def getVserver(client, vserver):
+        command = "getlbvserver"
+        arg = {'name':vserver}
+
+        try:
+            output = runCmd(client,command,**arg)
+        except RuntimeError, e:
+            raise RuntimeError(e)
+
+
+    def saveConfig(client):
+        command = "savensconfig"
+
+        try:
+            output = runCmd(client,command)
+        except RuntimeError, e:
+            raise RuntimeError(e)
