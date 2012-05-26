b12217cc3cb35f7dcb41ef375be1491870b6350d
Brian <xbglowx@gmail.com>
2012-05-25 23:35:31 -0700

    Created format print for dictionaries.

--- netscaler/format.py
+++ netscaler/format.py
@@ -3,3 +3,12 @@ def printList(list):
         print entry
 
     return 0
+
+def printDict(dict):
+    for key in sorted(dict.keys()):
+        print "%s: %s" % (key,dict[key])
+    
+    #for entry in dict.sort():
+    #    print "%s: %s" % (entry,dict[entry])
+
+    return 0
