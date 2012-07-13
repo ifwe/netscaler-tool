0becc8a6756e59a66a5c9dead9c985a0e155b6ed
Brian Glogower <bglogower@tagged.com>
2012-07-13 11:28:58 -0700

    Forgot to increase version to 1.3 after adding show all servers.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='netscaler-tool',
-    version='1.2',
+    version='1.3',
     packages = find_packages(),
 
     author = "Brian Glogower",
