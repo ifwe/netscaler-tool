4ce4acc154989e6d96c6b89df89772772b65a500
Brian Glogower <bglogower@tagged.com>
2013-04-15 14:06:36 -0700

    Increased release value.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='netscaler-tool',
-    version='1.4',
+    version='1.5',
     packages = find_packages(),
 
     author = "Brian Glogower",
